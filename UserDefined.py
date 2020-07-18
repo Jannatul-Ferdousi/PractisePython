a492c50c0ff4ba5d309950834f32edc34e9321cf
Jannatul-Ferdousi <ferdousi1709011@stud.kuet.ac.bd>
2020-07-18 13:18:35 +0600

    First Commit

--- UserDefined.py
+++ UserDefined.py
@@ -1,1 +1,12 @@
 print("Hello")
+# Define the function shout
+def shout():
+    """Print a string with three exclamation marks"""
+    # Concatenate the strings: shout_word
+    shout_word = 'congratulations' + '!!!'
+
+    # Print shout_word
+    print(shout_word)
+
+# Call shout
+shout()

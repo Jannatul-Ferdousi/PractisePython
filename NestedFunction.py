96cb8412475af747eddac8f67db14b94b51a9894
Jannatul-Ferdousi <61084978+Jannatul-Ferdousi@users.noreply.github.com>
2020-07-26 20:24:34 +0600

    Add files via upload

---
+++ NestedFunction.py
@@ -0,0 +1,12 @@
+# Define three_shouts
+def three_shouts(word1, word2, word3):
+    """Returns a tuple of strings
+    concatenated with '!!!'."""
+
+    # Define inner
+
+    # Return a tuple of strings
+    return (inner(word1),inner(word2), inner(word3) )
+
+# Call three_shouts() and print
+print(three_shouts('a', 'b', 'c'))
